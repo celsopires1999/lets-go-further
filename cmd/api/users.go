680a5793cbe7b9645279fc5c0ef320dabcc52342
celsopires1999 <celso.pires1999@gmail.com>
2024-09-21 12:28:09 -0300

    Recovering panics

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -60,16 +60,14 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	// Launch a goroutine which runs an anonymous function that sends the welcome email
-	go func() {
+	// Use the background helper to execute an anonymous function that sends the welcome
+	// email.
+	app.background(func() {
 		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
 		if err != nil {
-			// Importantly, if there is an error sending the email then we use the
-			// app.logger.PrintError() helper to manage it, instead of the
-			// app.serverErrorResponse() helper like before.
 			app.logger.PrintError(err, nil)
 		}
-	}()
+	})
 
 	// Write a JSON response containing the user data along with a 201 Created status
 	// code.
