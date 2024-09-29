2af45f0a97158187e5cd84511a8dfc103fec1c34
celsopires1999 <celso.pires1999@gmail.com>
2024-09-29 00:34:10 +0000

    Creating Custom Metrics

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
 	"flag"
 	"os"
 	"strings"
@@ -100,6 +101,8 @@ func main() {
 	defer db.Close()
 
 	logger.PrintInfo("database connection pool established", nil)
+
+	expvar.NewString("version").Set(version)
 
 	app := &application{
 		config: cfg,
